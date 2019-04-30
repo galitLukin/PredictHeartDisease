a757735008b1cbe97b9b944afc63488b8bba0b5f
Galit <glukin@mit.edu>
2019-04-29 22:28:31 -0400

    output measures

--- main.jl
+++ main.jl
@@ -28,16 +28,18 @@ end
 
 df = readtable("Framingham.csv", header=true, makefactors=true)
 srand(1)
-train, test =  splitobs(shuffleobs(df), at=0.05)
+train, test =  splitobs(shuffleobs(df), at=0.67)
 cTrain = buildC(train)
 train = normalizeData(train)
 test = normalizeData(test)
 measures = DataFrame( model = String[], trainORtest = String[], accuracy = Float64[], precision = Float64[], recall = Float64[])
 # Nominal
 measures = runNominalModel(cTrain, train, measures)
+writetable("measures1.csv",measures)
 # Lab Errors
 measures = runLabModel(cTrain, train, measures, 0.05)
 measures = runLabModel(cTrain, train, measures, 0.2)
+writetable("measures2.csv",measures)
 # Lies
 # measures = runModel(cTrain, train, "Lies", measures)
 # doctors error
